fc696c79a794f4b44a67fc41888491e9dc2e05ab
rahulsengupta0 <rahul@creditoracademy.com>
2025-08-26 12:01:40 +0530

    navbar and the video

--- container/about-page/Hero.tsx
+++ container/about-page/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
         playsInline
         className="absolute inset-0 w-full h-full object-cover z-0"
       >
-        <source src="/Features.mp4" type="video/mp4" />
+        <source src="/features.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       {/* --- Light Blue Overlay --- */}
